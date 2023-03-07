76e4d530cecd256c1be958799ae3a7c559bb9e24
Prince Prajapati <pprajapati2316@conestogac.on.ca>
2023-03-07 01:38:13 -0500

    Added Shopping page and Checkout Page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from products.models import Product
+
+
 # Create your views here.
 
 
 def index(request):
     context = {'products': Product.objects.all()}
     return render(request, 'home/index.html', context)
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product,Category
 
 
 # Create your views here.
@@ -11,3 +11,17 @@ def get_product(request, slug):
         return render(request, 'product/product.html', context={'product': product})
     except Exception as e:
         print(e)
+
+
+def category(request):
+    var = {'category': Category.objects.all()}
+    return request, var
+
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')
+
+
+def shop(request):
+    context = {'products': Product.objects.all()}
+    return render(request, 'shop/shop.html', context)
